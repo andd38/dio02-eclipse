78b66c7b650b75bc49b3cebed114319ec4a9c587
andre patriarca <andrepatriarca4@gmail.com>
2024-05-24 08:24:50 -0300

    error consertado

--- src/DesafioIphone/model/Main.java
+++ src/DesafioIphone/model/Main.java
@@ -1,5 +1,15 @@
 package DesafioIphone.model;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		Celular celular =  new Celular();
+		celular.adicionarNovaAba();
+		celular.atender();
+		celular.atualizarPagina();
+		celular.ExibirPagina();
+		celular.ligar();
+		
+		
+	}
 }
